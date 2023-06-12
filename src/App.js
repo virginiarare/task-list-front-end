49798bc6d53eacc02b2e6e1741c2f56f479bc8ba
Ansel Rognlie <ansel.rognlie@gmail.com>
2023-06-12 07:04:23 -0700

    wave 00 starting point

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const App = () => {
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div><TaskList tasks={TASKS} /></div>
       </main>
     </div>
   );
