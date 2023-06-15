e915b8e123c102e7407246b76ad653c1550bb730
Virginia Ramos <virginia.rare@gmail.com>
2023-06-15 13:09:20 -0500

    Wave 01

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React , { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import './Task.css';
 
-const Task = ({ id, title, isComplete }) => {
-  const buttonClass = isComplete ? 'tasks__item__toggle--completed' : '';
+const Task = (props) => {
+  const [completed, setCompleted] = useState(props.isComplete);
+
+  const toggleCompleted = () => {
+    setCompleted(!completed);
+  };
+
+  const buttonClass = completed ? 'tasks__item__toggle--completed' : '';
+
   return (
     <li className="tasks__item">
-      <button className={`tasks__item__toggle ${buttonClass}`}>{title}</button>
+      <button className={`tasks__item__toggle ${buttonClass}`} onClick={toggleCompleted}>
+        {props.title}
+      </button>
       <button className="tasks__item__remove button">x</button>
     </li>
   );
@@ -20,3 +29,25 @@ Task.propTypes = {
 };
 
 export default Task;
+
+
+// const Task = ({ id, title, isComplete }) => {
+//   const buttonClass = isComplete ? 'tasks__item__toggle--completed' : '';
+//   return (
+//     <li className="tasks__item">
+//       <button className={`tasks__item__toggle ${buttonClass}`}>{title}</button>
+//       <button className="tasks__item__remove button">x</button>
+//     </li>
+//   );
+// };
+
+// Task.propTypes = {
+//   id: PropTypes.number.isRequired,
+//   title: PropTypes.string.isRequired,
+//   isComplete: PropTypes.bool.isRequired,
+// };
+
+// export default Task;
+
+
+
