dfb3ed06587be5e2ab35fe1a0bf39a5cbf2ae539
Virginia Ramos <virginia.rare@gmail.com>
2023-06-22 02:34:41 -0500

    Wave 3

--- src/App.js
+++ src/App.js
@@ -1,35 +1,51 @@
-// App.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import TaskList from './components/TaskList';
 import './App.css';
 
-const App = () => {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      title: 'Mow the lawn',
-      isComplete: false,
-    },
-    {
-      id: 2,
-      title: 'Cook Pasta',
-      isComplete: true,
-    },
-  ]);
+const kBaseUrl = 'http://127.0.0.1:5000';
 
-  const toggleCompleted = (taskId) => {
-    setTasks((prevTasks) =>
-      prevTasks.map((task) => {
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const response = await axios.get(`${kBaseUrl}/tasks`);
+        setTasks(response.data);
+      } catch (error) {
+        console.error('Error fetching tasks:', error);
+      }
+    };
+
+    fetchTasks();
+  }, []);
+
+  const toggleCompleted = async (taskId) => {
+    try {
+      const updatedTasks = tasks.map((task) => {
         if (task.id === taskId) {
           return { ...task, isComplete: !task.isComplete };
         }
         return task;
-      })
-    );
+      });
+
+      setTasks(updatedTasks);
+
+      await axios.patch(`${kBaseUrl}/tasks/${taskId}/${tasks.find((task) => task.id === taskId).isComplete ? 'mark_incomplete' : 'mark_complete'}`);
+    } catch (error) {
+      console.error('Error updating task:', error);
+    }
   };
 
-  const deleteTask = (taskId) => {
-    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
+  const deleteTask = async (taskId) => {
+    try {
+      setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
+
+      await axios.delete(`${kBaseUrl}/tasks/${taskId}`);
+    } catch (error) {
+      console.error('Error deleting task:', error);
+    }
   };
 
   return (
@@ -47,4 +63,49 @@ const App = () => {
 };
 
 export default App;
+
+// const App = () => {
+//   const [tasks, setTasks] = useState([
+//     {
+//       id: 1,
+//       title: 'Mow the lawn',
+//       isComplete: false,
+//     },
+//     {
+//       id: 2,
+//       title: 'Cook Pasta',
+//       isComplete: true,
+//     },
+//   ]);
+
+//   const toggleCompleted = (taskId) => {
+//     setTasks((prevTasks) =>
+//       prevTasks.map((task) => {
+//         if (task.id === taskId) {
+//           return { ...task, isComplete: !task.isComplete };
+//         }
+//         return task;
+//       })
+//     );
+//   };
+
+//   const deleteTask = (taskId) => {
+//     setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
+//   };
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h1>Ada&apos;s Task List</h1>
+//       </header>
+//       <main>
+//         <div>
+//           <TaskList tasks={tasks} toggleCompleted={toggleCompleted} deleteTask={deleteTask} />
+//         </div>
+//       </main>
+//     </div>
+//   );
+// };
+
+// export default App;
 
